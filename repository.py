f47af91bb270959a54f8f689f2844ca365f3e60f
felipe <fpfarinhaki.atc@gmail.com>
2019-05-30 19:00:31 +0200

    Fix JSONStorage encoding

--- repository.py
+++ repository.py
@@ -2,28 +2,28 @@ from tinydb import TinyDB
 
 
 def movies():
-    return TinyDB('M3U_MOVIES.json')
+    return TinyDB('M3U_MOVIES.json', encoding='utf8', ensure_ascii=False)
 
 
 def channels():
-    return TinyDB('M3U_CHANNEL.json')
+    return TinyDB('M3U_CHANNEL.json', encoding='utf8', ensure_ascii=False)
 
 
 def series():
-    return TinyDB('M3U_SERIES.json')
+    return TinyDB('M3U_SERIES.json', encoding='utf8', ensure_ascii=False)
 
 
 def adult_movies():
-    return TinyDB('M3U_MOVIES_ADULT.json')
+    return TinyDB('M3U_MOVIES_ADULT.json', encoding='utf8', ensure_ascii=False)
 
 
 def other():
-    return TinyDB('M3U_OTHER.json')
+    return TinyDB('M3U_OTHER.json', encoding='utf8', ensure_ascii=False)
 
 
 def movie_data():
-    return TinyDB('MOVIE_DATA.json')
+    return TinyDB('MOVIE_DATA.json', encoding='utf8', ensure_ascii=False)
 
 
 def no_data_movies():
-    return TinyDB('NO_DATA_MOVIES.json')
+    return TinyDB('NO_DATA_MOVIES.json', encoding='utf8', ensure_ascii=False)
