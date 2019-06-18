014569d4a171818795025db5ba3659525f6ed4ab
felipe <fpfarinhaki.atc@gmail.com>
2019-06-18 12:57:05 +0200

    Fix duplication problem when listing series with no data

--- domain/MissingDataItem.py
+++ domain/MissingDataItem.py
@@ -2,3 +2,11 @@ class MissingDataItem:
     def __init__(self, name, poster_path):
         self.name = name
         self.poster_path = poster_path
+
+    def __eq__(self, o: object) -> bool:
+        if isinstance(o, MissingDataItem):
+            return self.name == o.name
+        return False
+
+    def __hash__(self) -> int:
+        return hash(self.name)
