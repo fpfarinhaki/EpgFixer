74be9295f2d6377e5748e0017786a141522eefb6
Felipe P. Farinhaki <fpfarinhaki.atc@gmail.com>
2019-07-11 12:35:05 +0000

    Update install file to include manual fixer installation

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 sudo pyinstaller ./application/M3uListManager.py --distpath /usr/local/bin/ --clean -F -n m3ulistmanager --exclude-module test
+
+sudo pyinstaller ./application/ManualFixer.py --distpath ~/m3u_list_manager/ --clean -F -n manualfixer --exclude-module test
