8807808acd777e05569048320747aaec87f9417f
felipe <fpfarinhaki.atc@gmail.com>
2019-07-17 16:27:05 +0200

    Change return logic for dateutils to fix UnboundedError

--- utils/dateutils.py
+++ utils/dateutils.py
@@ -4,12 +4,10 @@ from datetime import datetime
 
 def format_release_date(date):
     try:
-        release_date = datetime.strftime(datetime.strptime(date, '%Y-%m-%d'), '%d-%m-%Y')
+        return datetime.strftime(datetime.strptime(date, '%Y-%m-%d'), '%d-%m-%Y')
     except ValueError:
         logging.error("Release date not in proper format - {}".format(date))
-        release_date = date
+        return date
     except TypeError:
         logging.error("Error on type of date provided - {}".format(date))
-        release_date = ''
-
-    return release_date
+        return ''
